a6ffbcfe4a8e06806146bf309879a2f5a23d1487
Stephen Baity <steve.baity@gmail.com>
2020-03-11 18:12:57 -0700

    Able to display user's current location

--- GroupAlert/ViewController.swift
+++ GroupAlert/ViewController.swift
@@ -7,14 +7,57 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
+	@IBOutlet weak var mapView: MKMapView!
 
-	override func viewDidLoad() {
-		super.viewDidLoad()
-		// Do any additional setup after loading the view.
-	}
+	var locationManager = CLLocationManager()
+	var currentLocation = CLLocation()
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+		locationManager.requestWhenInUseAuthorization()
+		currentLocation = locationManager.location!
+		
+
+		if CLLocationManager.locationServicesEnabled() {
+			locationManager.delegate = self
+			locationManager.desiredAccuracy = kCLLocationAccuracyBest
+			locationManager.startUpdatingLocation()
+		}
+		
+		mapView.delegate = self
+		   mapView.mapType = .standard
+		   mapView.isZoomEnabled = true
+		   mapView.isScrollEnabled = true
+		mapView.showsCompass = true
+		
+		
+		let neighborhoodSpan = MKCoordinateSpan(latitudeDelta: 0.25, longitudeDelta: 0.25)
+		
+		let region = MKCoordinateRegion(center: currentLocation.coordinate, span: neighborhoodSpan)
+		mapView.setRegion(region, animated: true)
+		
+		
+		
+		
+		
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
 
 }
 
